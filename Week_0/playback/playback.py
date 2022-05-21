ea68a888b48aed76f5d154008ab41db706695827
Fernando Oliveira <fernandooliveira@live.com>
2022-05-21 19:45:59 -0300

    playback comentarios

--- Week_0/playback/playback.py
+++ Week_0/playback/playback.py
@@ -1,7 +1,12 @@
+# Recebe o input do usuário
 txt = input()
+# Percorre a string
 for i in range(len(txt)):
+    # Verifica se existe espaco no input digitado
     if txt[i] == " ":
+        # Caso exista espaco, substitui por "..."
         print("...", end="")
     else:
+        # Caso nao exista espaco, imprime o caractere
         print(txt[i], end="")
 print()   
